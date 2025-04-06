c04d8afa228dddf6bbcd60610385bb749f514b52
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-04-06 06:45:56 -0700

    1. Project created 2. UIElements added and configured

---
+++ LoginApp/GreatingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GreatingViewController.swift
+//  LoginApp
+//
+//  Created by OSIIOSdev on 06.04.2025.
+//
+
+import UIKit
+
+final class GreatingViewController: UIViewController {
+
+    // MARK: - @IBOutlets
+    
+    
+    // MARK: - Private properties
+    
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    // MARK: - @IBActions
+    
+    
+    // MARK: - Private methods
+
+}
---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by OSIIOSdev on 06.04.2025.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    // MARK: - @IBOutlets
+    
+    
+    // MARK: - Private properties
+    
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    // MARK: - @IBActions
+    
+    
+    // MARK: - Private methods
+
+}
+
